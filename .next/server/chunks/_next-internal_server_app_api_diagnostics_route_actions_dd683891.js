07f40d9c7374a455b9131b2ff3b9c66af03b8754
Om vataliya <omvataliya23@gmail.com>
2026-01-03 10:34:49 +0530

    Initial Coomit Code Push

---
+++ .next/server/chunks/_next-internal_server_app_api_diagnostics_route_actions_dd683891.js
@@ -0,0 +1,3 @@
+module.exports=[80278,s=>{"use strict";s.s([])}];
+
+//# sourceMappingURL=_next-internal_server_app_api_diagnostics_route_actions_dd683891.js.map
---
+++ app/onboarding/page.tsx
@@ -0,0 +1,5 @@
+import { OnboardingPage } from '@/components/OnboardingPage'
+
+export default function OnboardingRoute() {
+  return <OnboardingPage />
+}
